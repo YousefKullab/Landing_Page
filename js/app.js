079bb827426db63a1abfdeb5ae6b3178d9d48122
YousefKullab <the1yousef98@gmail.com>
2023-07-01 20:30:22 +0300

    createMenu function to create nav (menu) when DOM content loaded

--- js/app.js
+++ js/app.js
@@ -18,44 +18,47 @@
  * Great to have comments before crucial code sections within the procedure.
 */
 
-/**
- * Define Global Variables
- * 
-*/
+
+// Define Global Variables
+const sections = document.querySelectorAll("section");
+const navbar_list = document.getElementById("navbar__list");
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// Helper Functions
 
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
+// Main Functions
 // build the nav
+/*** Setps
+ * # Loop through each section and create a menu item for it
+ * # Get the id of the section and name of section from data-nav
+ * # Set the menu item attributes and content
+ * # Append the item to navList
+ */ 
+function createMenu(){
+    // Loop through each section and create a menu item for it
+    sections.forEach(function(section){
+        const listItem = document.createElement("li");
+        // Get the id of the section and title of section from data-nav
+        const sec_id = section.getAttribute('id');
+        const sec_title = section.getAttribute('data-nav');
+        // Set the menu item attributes and content
+        listItem.innerHTML = `<a class="menu__link" href="#${sec_id}">${sec_title}</a>`;
+        // Append the item to navList
+        navbar_list.appendChild(listItem);
+    });
+}
 
 
 // Add class 'active' to section when near top of viewport
-
-
 // Scroll to anchor ID using scrollTO event
 
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
+// Events
 // Build menu 
+document.addEventListener("DOMContentLoaded", createMenu);
 
 // Scroll to section on link click
-
 // Set sections as active
+
 
